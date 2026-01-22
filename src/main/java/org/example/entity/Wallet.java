f2bd87e25b31fd480d294b51242c78126fea4989
lenguyenthai-admin <lnt0995449235@gmail.com>
2026-01-22 16:23:44 +0700

    add wallet

--- src/main/java/org/example/entity/Wallet.java
+++ src/main/java/org/example/entity/Wallet.java
@@ -1,4 +1,34 @@
 package org.example.entity;
 
-public class Wallet {
+import org.example.utils.EntityWithId;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Wallet extends EntityWithId {
+
+    private Double balance;
+
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public Wallet(Double balance) {
+        this.balance = balance;
+    }
+
+    public Double getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void setBalance(Double balance) {
+        lock.lock();
+        try {
+            this.balance = balance;
+        } finally {
+            lock.unlock();
+        }
+    }
 }
